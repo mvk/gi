3770e7cdf794a65729be0255da1ac1b12c0f8623
esthercamilo <esthercamilo@gmail.com>
2015-02-09 15:59:06 -0200

    start

--- src/01-initsetup.py
+++ src/01-initsetup.py
@@ -1,1 +1,29 @@
-__author__ = 'esther'
+#################################
+#AUTHOR: ESTHER CAMILO          #
+#e-mail: esthercamilo@gmail.com #
+#################################
+import os
+
+finput = open("config.txt")
+folder = finput.readline().rstrip("\n")
+
+def makedir(namedir):
+	if not os.path.exists(namedir):
+		os.makedirs(namedir)
+
+#Levels
+
+l1=['ppi','reg','met','int']
+l2=['deg','bet','jc','complete']
+l3=['100','95','90','85']
+l4=['19-20','39']
+
+
+for a in l1:
+    for b in l2:
+        for c in l3:
+            for d in l4:
+                path =('%s/%s/%s/%s' %(a,b,c,d))
+                makedir(folder+path)
+
+
