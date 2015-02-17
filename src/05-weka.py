f441b5d0888eb546b77690b305d965521f735d8f
Max Kovgan <kovganm@gmail.com>
2015-02-17 22:49:01 +0200

    tabs to spaces

--- src/05-weka.py
+++ src/05-weka.py
@@ -21,17 +21,17 @@ end = 2
 
 # Convert csv to arff
 def convert():
-	print '***********CONVERTING FROM CSV TO ARFF - remove gene1 and gene2 attributes\n'
-	for a in l1:
-		for b in l2:
-			for c in l3:
-				for d in l4:
-					for e in l5:
-						path = (folder+'%s/%s/%s/%s/%s/' % (a, b, c, d, e))
-						lista_files = os.listdir(path + 'csv/')
-						for file in lista_files:
-							command = "java -cp "+wekalocation+" weka.filters.unsupervised.attribute.Remove -R 1,2 -i "+path+ "csv/" + file + " -o "+ path + "arff/" + file.replace('csv','arff')
-							os.system(command)
+    print '***********CONVERTING FROM CSV TO ARFF - remove gene1 and gene2 attributes\n'
+    for a in l1:
+        for b in l2:
+            for c in l3:
+                for d in l4:
+                    for e in l5:
+                        path = (folder+'%s/%s/%s/%s/%s/' % (a, b, c, d, e))
+                        lista_files = os.listdir(path + 'csv/')
+                        for file in lista_files:
+                            command = "java -cp "+wekalocation+" weka.filters.unsupervised.attribute.Remove -R 1,2 -i "+path+ "csv/" + file + " -o "+ path + "arff/" + file.replace('csv','arff')
+                            os.system(command)
 
 convert()
 
