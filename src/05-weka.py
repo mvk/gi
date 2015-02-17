d61d5b76b11521ad07cc8e086a9ef0e1dbbec084
Max Kovgan <kovganm@gmail.com>
2015-02-17 22:59:12 +0200

    make popen based launcher

--- src/05-weka.py
+++ src/05-weka.py
@@ -5,6 +5,7 @@
 
 import os
 import sys
+import subprocess
 
 finput = open("config.txt")
 folder = finput.readline().rstrip("\n")
@@ -19,13 +20,31 @@ l6 = ['csv']
 
 end = 2
 
+
 def run_cmd_with_system(wekalocation, path, fpath):
     command = "java -cp "+wekalocation+" weka.filters.unsupervised.attribute.Remove -R 1,2 -i "+path+ "csv/" + fpath + " -o "+ path + "arff/" + file.replace('csv','arff')
     os.system(command)
 
+
+def run_cmd_with_popen(wekalocation, path, fpath):
+    command_arr = [
+        "java -cp",
+        wekalocation,
+        "weka.filters.unsupervised.attribute.Remove",
+        "-R 1,2",
+        "-i", os.path.join(path, "csv", fpath),
+        "-o", os.path.join(path, "arff", fpath.replace('csv','arff'))
+    proc = subprocess.Popen(
+        command_arr,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE
+    )
+    return proc.communicate()
+
 # Convert csv to arff
 def convert():
     print '***********CONVERTING FROM CSV TO ARFF - remove gene1 and gene2 attributes\n'
+    results = []
     for a in l1:
         for b in l2:
             for c in l3:
@@ -34,7 +53,8 @@ def convert():
                         path = (folder+'%s/%s/%s/%s/%s/' % (a, b, c, d, e))
                         lista_files = os.listdir(path + 'csv/')
                         for fpath in lista_files:
-                            run_cmd_with_system(wekalocation, path, fpath)
+                            res = run_cmd_with_popen(wekalocation, path, fpath)
+                            results.append(res.returncode)
 
 convert()
 
